e7769de4e16e8b752233db7140b820ef0be37b3a
hyc-hongwei <hyc-hongwei@users.noreply.github.com>
2016-10-17 13:41:15 +0800

    Update PreviewViewController.m
    修复多选照片出现选择的数组到达上限时，选不了照片问题

--- multiSelectImagePickerControllerDemo/multiSelectImagePickerControllerDemo/MultiSelectUnit/PreviewViewController.m
+++ multiSelectImagePickerControllerDemo/multiSelectImagePickerControllerDemo/MultiSelectUnit/PreviewViewController.m
@@ -85,7 +85,7 @@
     [self.navigationController popViewControllerAnimated:YES];
     //[self dismissViewControllerAnimated:YES completion:nil];
 }
--(void)selectButtonClick:(UIButton *)selectButton
+/*-(void)selectButtonClick:(UIButton *)selectButton
 {
     if(_collectionView.isDecelerating){
         return;
@@ -93,6 +93,78 @@
     LSYAlbumModel *model = self.allAssets[_selectedItem];
     model.isSelect = !model.isSelect;
     model.indexPath =[NSIndexPath indexPathForItem:_selectedItem inSection:0];
+    self.previewNavBar.selectButton.selected = model.isSelect;
+    if (model.isSelect) {
+        //[self.AlbumCollection selectItemAtIndexPath:model.indexPath animated:NO scrollPosition:UICollectionViewScrollPositionNone];
+        [self.assets addObject:model];
+        //[self.AlbumCollection.delegate collectionView:self.AlbumCollection didSelectItemAtIndexPath:model.indexPath];
+        if (self.delegate &&[self.delegate respondsToSelector:@selector(didSelectModel:)]) {
+            [self.delegate didSelectModel:model];//处理上一页的活动
+        }
+        
+        
+    }
+    else
+    {
+        //[self.AlbumCollection deselectItemAtIndexPath:model.indexPath animated:NO];
+        [self.assets removeObject:model];
+        //[self.AlbumCollection.delegate collectionView:self.AlbumCollection didDeselectItemAtIndexPath:model.indexPath];
+        if (self.delegate && [self.delegate respondsToSelector:@selector(didSelectModel:)]) {
+            [self.delegate didSelectModel:model];//处理上一页的活动
+        }
+    }
+    [self.previewToolBar setSendNumber:(int)self.assets.count/*self.AlbumCollection.indexPathsForSelectedItems.count*/];
+}*/
+    -(void)selectButtonClick:(UIButton *)selectButton
+{
+    /*if (self.maxminumNumber) {
+        if (!(self.maxminumNumber>self.assets.count)) {
+            
+            UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"提示" message:[NSString stringWithFormat:@"最多只能选%d张照片",(int)self.maxminumNumber] delegate:nil cancelButtonTitle:@"确定" otherButtonTitles: nil];
+            [alertView show];
+            return;
+        }
+    }*/
+    
+    if(_collectionView.isDecelerating){
+        return;
+    }
+    LSYAlbumModel *model = self.allAssets[_selectedItem];
+    
+    if(self.assets.count<self.maxminumNumber)
+    {//当选中数量小于总限制数
+        [self selectButtonClickAgain:model];
+    }else if(self.assets.count>self.maxminumNumber){
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"提示" message:[NSString stringWithFormat:@"最多只能选%d张照片",(int)self.maxminumNumber] delegate:nil cancelButtonTitle:@"确定" otherButtonTitles: nil];
+        [alertView show];
+        return;
+    }else if(self.assets.count==self.maxminumNumber) {
+        //当选中数量==总限制数
+        BOOL isInclude = NO;
+        for(int i=0;i<self.assets.count;i++){
+            LSYAlbumModel *tmpM = self.assets[i];
+            if([tmpM isEqual:model]){
+                isInclude = YES;
+                break;
+            }
+        }
+        if(isInclude){
+            //已被选中
+            [self selectButtonClickAgain:model];
+        }else{
+            UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"提示" message:[NSString stringWithFormat:@"最多只能选%d张照片",(int)self.maxminumNumber] delegate:nil cancelButtonTitle:@"确定" otherButtonTitles: nil];
+            [alertView show];
+            return;
+            
+        }
+    }
+    
+}
+
+-(void)selectButtonClickAgain:(LSYAlbumModel *)model
+{
+     model.indexPath =[NSIndexPath indexPathForItem:_selectedItem inSection:0];
+    model.isSelect = !model.isSelect;
     self.previewNavBar.selectButton.selected = model.isSelect;
     if (model.isSelect) {
         //[self.AlbumCollection selectItemAtIndexPath:model.indexPath animated:NO scrollPosition:UICollectionViewScrollPositionNone];
